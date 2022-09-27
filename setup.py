ca88e12d2bd855518763f70ff45bd2cac7122ce3
Subhang Mall <subhangmall@Subhangs-MacBook-Pro.local>
2022-09-27 12:16:00 -0700

    did stuff

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='pythagoreandistcalc',
-    version='1.1',
+    version='2.0',
     license='MIT',
     author="randomsoftwareshack",
     author_email='lolimnotputtingmyrealemail@poopmap.net',
-    packages=find_packages('pythagdistcalc'),
-    package_dir={'': 'pythagdistcalc'},
+    packages=find_packages('pythagoreandistcalc'),
+    package_dir={'': 'pythagoreandistcalc'},
     url='https://github.com/randomsoftwareshack/pythag-dist-calc',
     keywords='distance formula',
     install_requires=[
